1436a180cb0088b53d53de83edcc18148f845807
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-30 18:38:56 -0300

    feature: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,26 @@
 import Head from 'next/head'
 
+import styles from '../styles/pages/home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | ignews</title>
+        <title>Home | ignews</title>
       </Head>
 
-      <h1>
-        Hello World!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome </span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="" />
+      </main>
     </>
     
   )
