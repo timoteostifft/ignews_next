a90544e0e68d4b753809e5b05a755ba0990c0a33
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-31 01:17:27 -0300

    feature: implement subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import SubscribeButton from '../components/SubscribeButton'
 
 import styles from '../styles/pages/home.module.scss'
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="" />
