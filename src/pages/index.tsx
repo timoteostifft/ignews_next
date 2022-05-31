3d3a35fc4bf423def5a1badc7c28ff1027449025
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-31 10:22:01 -0300

    refactor: update index page to static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import SubscribeButton from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1L5IytAXQT3jfDk8eb9JX5ij')
 
