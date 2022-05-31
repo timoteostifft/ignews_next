68d154987517d0952c25912d67591119b463bd75
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-31 09:37:30 -0300

    feature: create subscribe button props

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 import styles from './styles.module.scss'
 
-const SubscribeButton: React.FC = () => {
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
   return (
     <button className={styles.subscribeButton}>
       Subscribe now
