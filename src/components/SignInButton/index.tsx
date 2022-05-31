7a750b100d0a3e24ab046a13f102ed258808ef88
Timóteo Stifft <timoteostifft@gmail.com>
2022-05-31 15:30:29 -0300

    feature: implement use session next auth hook

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -4,24 +4,26 @@ import styles from './styles.module.scss'
 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession } from 'next-auth/react'
 
 const SignInButton: React.FC = () => {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type='button'
       className={styles.signInButton}
     >
       <FaGithub color="#04d361" />
-      Sign in with Github
+      {session.user.name}
       <FiX color='#737380' className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type='button'
       className={styles.signInButton}
+      onClick={()=>signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with Github
