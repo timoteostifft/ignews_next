25266bd31de82936c239d84d1c1c31f4a18e8729
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-03 13:38:09 -0300

    feature: create subscription method on subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
 const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
+
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button className={styles.subscribeButton}>
+    <button 
+      className={styles.subscribeButton}
+      type='button'
+      onClick={handleSubscribe}
+      >  
       Subscribe now
     </button>
   )
