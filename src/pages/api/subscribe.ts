548abb9abb3283867831e4c2de87ab6d9ed36e86
Timóteo Stifft <timoteostifft@gmail.com>
2022-06-03 14:46:21 -0300

    feature: create prevents existent user verification

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,19 +1,57 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb';
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if(request.method === 'POST') {
 
     const session = await getSession({ req: request })
 
-    const StripeCustomer = await stripe.customers.create({
-      email: session.user.email
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
 
+    let customerId = user.data.stripe_customer_id
+
+    if(!customerId) {
+      const StripeCustomer = await stripe.customers.create({
+        email: session.user.email
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: StripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = StripeCustomer.id
+    }
+
+  
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: StripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
